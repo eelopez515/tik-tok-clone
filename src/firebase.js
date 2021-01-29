93a4925dd474f4cb1e7c3444e527b4097682ddb2
eelopez515 <eelopez15@gmail.com>
2021-01-29 18:50:37 -0500

    Tik-Tok-Clone initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJMgFNRN0MTuN9HmGrLkX1tuaU0TjbNnU",
+    authDomain: "tik-tok-clone-5e16b.firebaseapp.com",
+    projectId: "tik-tok-clone-5e16b",
+    storageBucket: "tik-tok-clone-5e16b.appspot.com",
+    messagingSenderId: "1089887765561",
+    appId: "1:1089887765561:web:4b30ef8389fa00bb7379e8",
+    measurementId: "G-NHJQPNC0J2"
+  };
